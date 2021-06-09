78533be37096b4687b7a43dc64d2986a3c12cfd8
emidah <emil.m.dahlberg@gmail.com>
2021-06-10 02:30:11 +0300

    use metadata requests for validation

--- random_street_view.py
+++ random_street_view.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import random
 import sys
+import json
 from urllib.request import urlretrieve
 
 import shapefile  # pip install pyshp
@@ -20,10 +21,10 @@ except ImportError:
 # 25,000 image requests per 24 hours
 # See https://developers.google.com/maps/documentation/streetview/
 API_KEY = "INSERT_YOUR_API_KEY_HERE"
-GOOGLE_URL = (
-    "http://maps.googleapis.com/maps/api/streetview?sensor=false&"
-    "size=640x640&key=" + API_KEY
-)
+GOOGLE_IMG_URL = ("http://maps.googleapis.com/maps/api/streetview?sensor=false&"
+              "size=640x640&key=" + API_KEY)
+GOOGLE_METADATA_URL = ("https://maps.googleapis.com/maps/api/streetview/metadata?"
+              "size=640x640&key=" + API_KEY)
 
 IMG_PREFIX = "img_"
 IMG_SUFFIX = ".jpg"
@@ -33,7 +34,7 @@ parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
 )
 parser.add_argument(
-    "-n", "--images-wanted", type=int, default=10, help="Number of images wanted"
+    "-n", "--images-wanted", type=int, default=1, help="Number of images wanted"
 )
 parser.add_argument("country", help="ISO 3166-1 Alpha-3 Country Code")
 parser.add_argument(
@@ -94,31 +95,40 @@ for i, record in enumerate(sf.records()):
         break
 
 print("Getting images")
-attempts, country_hits, imagery_hits, imagery_misses = 0, 0, 0, 0
+imagery_hits = 0
 MAX_URLS = 25000
 
 if not os.path.exists(args.country):
     os.makedirs(args.country)
 
+attempts = 0
 try:
     while True:
-        attempts += 1
+        attempts +=1
         rand_lat = random.uniform(min_lat, max_lat)
         rand_lon = random.uniform(min_lon, max_lon)
         # print attempts, rand_lat, rand_lon
         # Is (lat,lon) inside borders?
         if point_inside_polygon(rand_lon, rand_lat, borders):
-            print("  In country")
-            country_hits += 1
+            print(" In country")
             lat_lon = str(rand_lat) + "," + str(rand_lon)
             outfile = os.path.join(args.country, IMG_PREFIX + lat_lon + IMG_SUFFIX)
-            url = GOOGLE_URL + "&location=" + lat_lon
+            meta_url = GOOGLE_METADATA_URL + "&location=" + lat_lon
+            img_url = GOOGLE_IMG_URL + "&location=" + lat_lon
             if args.heading:
-                url += "&heading=" + args.heading
+                img_url += "&heading=" + args.heading
+                meta_url += "&heading=" + args.heading
             if args.pitch:
-                url += "&pitch=" + args.pitch
+                img_url += "&pitch=" + args.pitch
+                meta_url += "&pitch=" + args.pitch
             try:
-                urlretrieve(url, outfile)
+                meta_res = urllib.urlopen(meta_url)
+                meta_body = meta_res.read()
+                meta_json = json.loads(meta_body.decode("utf-8"))
+                if 'status' in meta_json and meta_json['status'] == 'ZERO_RESULTS':
+                    pass
+                else:
+                    urllib.urlretrieve(img_url, outfile)
             except KeyboardInterrupt:
                 sys.exit("exit")
             if os.path.isfile(outfile):
@@ -128,21 +138,16 @@ try:
                 print(color)
                 if color[0] == "#e3e2dd" or color[0] == "#e3e2de":
                     print("    No imagery")
-                    imagery_misses += 1
                     os.remove(outfile)
                 else:
                     print("    ========== Got one! ==========")
                     imagery_hits += 1
                     if imagery_hits == args.images_wanted:
                         break
-            if country_hits == MAX_URLS:
-                break
 except KeyboardInterrupt:
     print("Keyboard interrupt")
 
 print("Attempts:\t", attempts)
-print("Country hits:\t", country_hits)
-print("Imagery misses:\t", imagery_misses)
 print("Imagery hits:\t", imagery_hits)
 
 # End of file
