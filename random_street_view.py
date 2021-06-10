851189b9128fb4900b0dcfb13964d269980f3f94
emidah <emil.m.dahlberg@gmail.com>
2021-06-10 03:03:12 +0300

    bump up the radius

--- random_street_view.py
+++ random_street_view.py
@@ -113,7 +113,7 @@ try:
             #print(" In country")
             lat_lon = str(rand_lat) + "," + str(rand_lon)
             outfile = os.path.join(args.country, IMG_PREFIX + lat_lon + IMG_SUFFIX)
-            meta_url = GOOGLE_METADATA_URL + "&radius=2000&location=" + lat_lon
+            meta_url = GOOGLE_METADATA_URL + "&radius=20000&location=" + lat_lon
             
             if args.heading:
                 meta_url += "&heading=" + args.heading
